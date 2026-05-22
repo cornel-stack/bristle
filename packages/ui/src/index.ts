fad87eadaa7e11f0376432034655c0c612d2644c
Cornel Amala <amcornelamala@gmail.com>
2026-05-22 04:32:07 +0300

    feat(ui): export ProblemCardCompact from barrel and subpath

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,7 @@
 export { ProblemCardFull } from "./problem-card-full";
 export type { ProblemCardFullProps, CategoryColor, SourceKey } from "./problem-card-full";
+export { ProblemCardCompact } from "./problem-card-compact";
+export type { ProblemCardCompactProps } from "./problem-card-compact";
 export { Sparkline, buildSparklinePath } from "./sparkline";
 export type { SparklineProps } from "./sparkline";
 export { SourceIcon, SOURCE_LABELS } from "./source-icons";
